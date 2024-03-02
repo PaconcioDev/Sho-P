a76bed365e6df34668e6133e1bbd4aee8285f162
Francisco Gomez <pgomezdev@gmail.com>
2024-03-01 20:20:15 -0500

    Related products and desktop product detail

--- src/pages/ProductDetail/ProductDetail.jsx
+++ src/pages/ProductDetail/ProductDetail.jsx
@@ -2,11 +2,11 @@ import "./ProductDetail.css";
 import { useContext } from "react";
 import { ProductsContext } from "../../context/ProductsContext";
 import { useParams } from "react-router-dom";
+import { Card } from "../../components/Card/Card.jsx";
 import { normalizeString } from "../../utils/normalizeString.js";
 
 function ProductDetail() {
   const { products } = useContext(ProductsContext);
-  console.log(products);
   const { productName } = useParams();
 
   const product = products.find(
@@ -19,22 +19,39 @@ function ProductDetail() {
         <section className="product__image-container">
           <img src={product?.image} />
         </section>
-        <section className="product__title-info">
-          <h1 className="product__name">{product?.name}</h1>
-          <span className="product__price">$ {product?.price}</span>
-        </section>
-        <section className="product__form-container">
-          <form action="">
+        <div className="product__info-container">
+          <section className="product__title-info">
+            <h1 className="product__name">{product?.name}</h1>
+            <span className="product__price">$ {product?.price}</span>
+          </section>
+          <section className="product__purchase-info">
             <button className="product__btn" type="submit">
               ADD TO CART
             </button>
-          </form>
-        </section>
-        <section className="product__desc">
-          <p>{product?.description}</p>
-        </section>
+            <p className="product__desc">{product?.description}</p>
+          </section>
+        </div>
       </main>
-      <aside>{/* TODO: Related Products */}</aside>
+      <aside className="related-products__container">
+        <hr />
+        <h2 className="related-products__title">Related Products</h2>
+        {/* //TODO: Make slider without scrollbar for desktop and mobile */}
+        <section className="related-products__slider">
+          {products
+            .filter(
+              (p) =>
+                p.name
+                  .toLowerCase()
+                  .split(" ")
+                  .some((word) =>
+                    product.name.toLowerCase().split(" ").includes(word)
+                  ) && p.name !== product.name
+            )
+            .map((p) => (
+              <Card key={p.id} data={p} />
+            ))}
+        </section>
+      </aside>
     </>
   );
 }
